e70655414c7a6bc3ef7b9416a099b049d60dd814
SilasReinagel <silas.reinagel@gmail.com>
2020-01-07 22:54:42 -0700

    Added Office assets. Experimented with some basic walking.

---
+++ src/OfficeSim/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private Vector3 followDistance;
+
+    void Awake()
+    {
+        followDistance = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        var targetPos = target.position + followDistance;
+        transform.position = Vector3.Lerp(transform.position, targetPos, 0.05f);
+    }
+}
---
+++ src/OfficeSim/Assets/GoToNavigationTarget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class GoToNavigationTarget : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float epsilon = 0.2f;
+
+    private NavMeshAgent _agent;
+
+    void Awake() => _agent = GetComponent<NavMeshAgent>();
+    
+    void Start()
+    {
+        _agent.destination = destination.position;
+        animator.SetBool("isWalking", true);
+    }
+
+    void Update()
+    {
+        var flatPos = new Vector3(transform.position.x, 0, transform.position.z);
+        var flatDestPos = new Vector3(destination.position.x, 0, destination.position.z);
+        if (Vector3.Distance(flatPos, flatDestPos) < epsilon)
+        {
+            animator.SetBool("isWalking", false);
+            enabled = false;
+        }
+    }
+}
