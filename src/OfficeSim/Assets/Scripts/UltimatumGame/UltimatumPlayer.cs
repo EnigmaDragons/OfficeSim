5284563087e55f607fd9d750578bfebf83eccee7
Silas Reinagel <silas.reinagel@carvana.com>
2020-01-08 17:26:43 -0700

    Create simple Top-Down Room. Closes #1

--- src/OfficeSim/Assets/Scripts/UltimatumGame/UltimatumPlayer.cs
+++ src/OfficeSim/Assets/Scripts/UltimatumGame/UltimatumPlayer.cs
@@ -1,5 +1,10 @@
+using System.Threading;
+
 public sealed class UltimatumPlayer
 {
+    private static int NextId = 1;
+
+    public int Id { get; } = Interlocked.Increment(ref NextId);
     public UltimatumStrategy Strategy { get; }
     public UltimatumPlayerState State { get; } = new UltimatumPlayerState();
 
